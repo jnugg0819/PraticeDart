2caee6b05039f3880ea2fead90231ddf3ab704dc
Olio <jnugg0819@users.noreply.github.com>
2025-03-20 18:21:44 +0900

    Order 클래스수정
    - 기존 주문 리스트를 애초에 변형하여 가져옴
    - getProductPriceMap putIfAbsent사용 으로 기존 값이 비어있더라도 값 넣어주고 사용

--- lib/syntax-exercises/order.dart
+++ lib/syntax-exercises/order.dart
@@ -3,64 +3,44 @@ class Order {
 
   Order(this.orderSheet);
 
-  List<String> getOrderList() {
-    List<String> orderList = orderSheet
+  List<List<String>> getOrderList() {
+    return orderSheet
         .split(";")
-        .map((e) => e.trim()) // 각 아이템마다 빈값이 있을수 있으므로.
-        .toList()
-        .where((e) => e.isNotEmpty) // 빈 요소는 리스트에 넣어주지 않습니다.
-        .toList();
-
-    // 만약 주문목록 내부 각 아이템 요소가 3이 아닐경우엔 Exception.
-    for (var item in orderList) {
-      List<String> specificOrder = item.split(",");
+        .map((e) => e.trim())
+        .where((e) => e.isNotEmpty)
+        .map((e) {
+      List<String> specificOrder = e.split(",");
       if (specificOrder.length != 3) {
-        throw FormatException("Invalid order format");
+        throw FormatException("Invalid order format: $e");
       }
-    }
-
-    return orderList;
+      return specificOrder;
+    }).toList();
   }
 
   Map<String, int> getProductPriceMap() {
     Map<String, int> productPrice = {};
 
-    List<String> orderList = getOrderList();
-
-    for (var item in orderList) {
-      List<String> specificOrder = item.split(",");
-
-      int increment = (int.tryParse(specificOrder[1]) ?? 0) *
-          (int.tryParse(specificOrder[2]) ?? 0);
-
-      if (productPrice.containsKey(specificOrder[0])) {
-        productPrice[specificOrder[0]] =
-            (productPrice[specificOrder[0]] ?? 0) + increment;
-      } else {
-        productPrice[specificOrder[0]] = increment;
-      }
+    for (var specificOrder in getOrderList()) {
+      String product = specificOrder[0];
+      int price = int.tryParse(specificOrder[1]) ?? 0;
+      int quantity = int.tryParse(specificOrder[2]) ?? 0;
+      int increment = price * quantity;
+      productPrice.putIfAbsent(product, () => 0);
+      productPrice[product] = productPrice[product]! + increment;
     }
 
     return productPrice;
   }
 
   Set<String> getProductOrderList() {
-    List<String> orderList = getOrderList();
-    return orderList.map((e) => e.split(",").first).toSet();
+    return getOrderList().map((e) => e[0]).toSet();
   }
 
-  int getProductOrderCount() {
-    return getOrderList().length;
-  }
+  int getProductOrderCount() => getOrderList().length;
 
-  int getPriceEachProduct(String name) {
-    return getProductPriceMap()[name] ?? 0;
-  }
+  int getPriceEachProduct(String name) => getProductPriceMap()[name] ?? 0;
 
-  int getTotalPriceOfProducts() {
-    Map<String, int> productPriceMap = getProductPriceMap();
-    return productPriceMap.sumValues();
-  }
+  int getTotalPriceOfProducts() => getProductPriceMap().sumValues();
 }
 
 extension SumExtension on Map<String, int> {
